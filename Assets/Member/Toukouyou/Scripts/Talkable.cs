6f8756ce208ce433d610d1dfa707f616fbedb612
TangXingYe0125 <ttc2350dg0006@edu.tech.ac.jp>
2024-12-06 18:30:30 +0900

    1
    コライーダ使わず、受注できるようになりました

--- Assets/Member/Toukouyou/Scripts/Talkable.cs
+++ Assets/Member/Toukouyou/Scripts/Talkable.cs
@@ -22,6 +22,10 @@ public class Talkable : MonoBehaviour
     [SerializeField] private GameObject Player;
     private RaycastHit2D hit;
     private Ray ray;
+
+    private Transform NPC_Pos;
+    [SerializeField] private float _talkableSizeX;
+    [SerializeField] private float _talkableSizeY;
     private void Start()
     {
         _NPC_Quest = GetComponent<NPC_Quest>();
@@ -35,6 +39,7 @@ public class Talkable : MonoBehaviour
             _questionText_rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 320);
             _questionText.text = "Help Me";//"ç¢Ç¡ÇΩÇ»...";//Change text
             DialogManager.instance._NPC_Quest = GetComponent<NPC_Quest>();//let Player get quest detail from this Npc
+            NPC_Pos = this.transform;
             Zoom.instance._NPC = this.gameObject;
         }
         else if (CalculateDistance() > 4 )
@@ -44,6 +49,7 @@ public class Talkable : MonoBehaviour
             _questionText_rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 200);
             _questionText.text = "?";//Change text
             DialogManager.instance._NPC_Quest = null;
+            NPC_Pos = null;
             Zoom.instance._NPC = null;
         }
 
@@ -52,25 +58,41 @@ public class Talkable : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(0))
             {
-                ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                if (Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity).collider == null)
+                Vector3 mousePosition = Input.mousePosition;
+                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, Camera.main.nearClipPlane));
+                float x = worldPosition.x;
+                float y = worldPosition.y;
+
+                if (x < NPC_Pos.position.x + _talkableSizeX && x > NPC_Pos.position.x - _talkableSizeX &&
+                   y < NPC_Pos.position.y + _talkableSizeY && y > NPC_Pos.position.y - _talkableSizeY)
                 {
-                    return;
-                }
-                else
-                {
-                    hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
-                }
-                if (hit.collider.gameObject.tag == "NPC")
-                {
+                    Debug.Log(NPC_Pos);
                     DialogManager.instance._testClick = false;
                     if (!EventSystem.current.IsPointerOverGameObject())
                     {
                         StartCoroutine(Zoom.instance.ZoomIn());
                     }
                 }
+
+                //ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                //if (Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity).collider == null)
+                //{
+                //    return;
+                //}
+                //else
+                //{
+                //    hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+                //}
+                //if (hit.collider.gameObject.tag == "NPC")
+                //{
+                //    DialogManager.instance._testClick = false;
+                //    if (!EventSystem.current.IsPointerOverGameObject())
+                //    {
+                //        StartCoroutine(Zoom.instance.ZoomIn());
+                //    }
+                //}
             }
-            
+
             if (Zoom.instance._isDone == true)
             {
                 if (DialogManager.instance.CheckQuestStatus() == "Accepted" || DialogManager.instance.CheckQuestStatus() == "Waiting")
