d2420b0ced31448c6d7e7ad35eb4f9ba6998b3ff
TangXingYe0125 <ttc2350dg0006@edu.tech.ac.jp>
2024-12-14 02:03:31 +0900

    1
    1

--- Assets/Member/Toukouyou/InfiniteScroll.cs
+++ Assets/Member/Toukouyou/InfiniteScroll.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,20 +11,19 @@ public class InfiniteScroll : MonoBehaviour
     public RectTransform _content_rtf;
     public GridLayoutGroup _gridLayoutGroup;
     public RectTransform[] _timeList;
-
+    private int _addNum;
     private bool _isUpdated;
     private Vector2 _oldVelocity;
+
+    private bool _isSnapped;
+    float _snapSpeed;
+    public float _snapForce;
     void Start()
     {
         _oldVelocity = Vector2.zero;
         _isUpdated = false;
-
-        int _addNum = Mathf.CeilToInt(_viewPort_rtf.rect.height / (_gridLayoutGroup.cellSize.y + _gridLayoutGroup.spacing.y));
-        for(int i = 0;i < _addNum;i++)
-        {
-            RectTransform rtf =Instantiate(_timeList[i % _timeList.Length], _content_rtf);
-            rtf.SetAsLastSibling();
-        }
+        _isSnapped = false;
+        _addNum = Mathf.CeilToInt(_viewPort_rtf.rect.height / (_gridLayoutGroup.cellSize.y + _gridLayoutGroup.spacing.y));
         for (int i = 0; i < _addNum; i++)
         {
             int j = _timeList.Length - 1 - i;
@@ -34,35 +34,99 @@ public class InfiniteScroll : MonoBehaviour
             RectTransform rtf = Instantiate(_timeList[j], _content_rtf);
             rtf.SetAsFirstSibling();
         }
-
         _content_rtf.localPosition = new Vector2(_content_rtf.localPosition.x, (_gridLayoutGroup.cellSize.y + _gridLayoutGroup.spacing.y) * _addNum);
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(_isUpdated)
+        if (_isUpdated)
         {
             _isUpdated = false;
             _scrollRect.velocity = _oldVelocity;
         }
 
-        if(_content_rtf.localPosition.y < 0)
+        if (_content_rtf.localPosition.y < 0)
         {
             Canvas.ForceUpdateCanvases();
             _oldVelocity = _scrollRect.velocity;
-            _content_rtf.localPosition += new Vector3(0,_timeList.Length * (_gridLayoutGroup.cellSize.y + _gridLayoutGroup.spacing.y),0);
+            _content_rtf.localPosition += new Vector3(0, _timeList.Length * (_gridLayoutGroup.cellSize.y + _gridLayoutGroup.spacing.y), 0);
             _isUpdated = true;
         }
 
-        if(_content_rtf.localPosition.y > (_gridLayoutGroup.cellSize.y + _gridLayoutGroup.spacing.y) * _timeList.Length)
+        if (_content_rtf.localPosition.y > (_gridLayoutGroup.cellSize.y + _gridLayoutGroup.spacing.y) * _timeList.Length)
         {
             Canvas.ForceUpdateCanvases();
             _oldVelocity = _scrollRect.velocity;
             _content_rtf.localPosition -= new Vector3(0, _timeList.Length * (_gridLayoutGroup.cellSize.y + _gridLayoutGroup.spacing.y), 0);
             _isUpdated = true;
         }
-        Debug.Log(_timeList.Length * (_gridLayoutGroup.cellSize.y + _gridLayoutGroup.spacing.y));
+        
+        int currentTime =  (Mathf.RoundToInt(_content_rtf.localPosition.y / (_gridLayoutGroup.cellSize.y + _gridLayoutGroup.spacing.y))-_addNum + _timeList.Length)% _timeList.Length;
+
+        var a = _timeList[currentTime].GetComponent<TextMeshProUGUI>();
+        var b = _timeList[(currentTime + 1) % _timeList.Length].GetComponent<TextMeshProUGUI>();
+        var c = _timeList[(currentTime + 2) % _timeList.Length].GetComponent<TextMeshProUGUI>();
+        Debug.Log(currentTime);
+        //Debug.Log("a:" + _timeList[currentTime]);
+        //Debug.Log("b:" + _timeList[(currentTime + 1) % _timeList.Length]);
+        //Debug.Log("c:" + _timeList[(currentTime + 2) % _timeList.Length]);
+        if (a.fontSize == 36 && c.fontSize == 36)
+        {
+            if (b.fontSize < 80)
+            {
+                b.fontSize += 11;
+            }
+
+        }
+        else
+        {
+            a.fontSize = 36;
+            c.fontSize = 36;
+        }
+        var ba = GameObject.Find("23(Clone)").GetComponent<TextMeshProUGUI>();
+        var sd = GameObject.Find("24(Clone)").GetComponent<TextMeshProUGUI>();
+        if (currentTime ==21)
+        {
+           
+            if (ba.fontSize < 80)
+            {
+                ba.fontSize += 11;
+            }
+        }
+        else
+        {
+            ba.fontSize = 36;
+        }
+        if(currentTime ==22)
+        {
+            if (sd.fontSize < 80)
+            {
+                sd.fontSize += 11;
+            }
+        }
+        else
+        {
+            sd.fontSize = 36;
+        }
+
+        if (_scrollRect.velocity.magnitude < 50 && !_isSnapped)
+        {
+            _scrollRect.velocity = Vector2.zero;
+            _snapSpeed += Time.deltaTime * _snapForce;
+            _content_rtf.localPosition = new Vector3(_content_rtf.localPosition.x,
+                                                    Mathf.MoveTowards(_content_rtf.localPosition.y, ((currentTime * (_gridLayoutGroup.cellSize.y + _gridLayoutGroup.spacing.y) + _content_rtf.rect.height) % _content_rtf.rect.height + (_gridLayoutGroup.cellSize.y + _gridLayoutGroup.spacing.y) * _addNum) % (_content_rtf.rect.height + _gridLayoutGroup.spacing.y -  _addNum *(_gridLayoutGroup.cellSize.y + _gridLayoutGroup.spacing.y)), _snapSpeed),
+                                                     _content_rtf.localPosition.z);
+            if (_content_rtf.localPosition.y == (_gridLayoutGroup.cellSize.y + _gridLayoutGroup.spacing.y) + (_gridLayoutGroup.cellSize.y + _gridLayoutGroup.spacing.y) * _addNum)
+            {
+                _isSnapped = true;
+            }
+        }
+
+        if (_scrollRect.velocity.magnitude > 50)
+        {
+            _isSnapped = false;
+            _snapSpeed = 0;
+        }
+
     }
 }
