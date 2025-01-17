34759caa9e93e16a2a9699cb53112c4ab15044b2
saitoyu1314 <ttc2350dg0004@edu.tech.ac.jp>
2025-01-17 17:10:17 +0900

    0117草むしり完成形

---
+++ Assets/Member/Saitoyuu/Scripete/anchor_View.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class anchor_View : MonoBehaviour
+{
+    [SerializeField] GameObject _nextNumberGameObject;
+    [SerializeField] GameObject _endNumberGameObject;
+    public int _count = 0;
+    Grass _grass;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject obj = GameObject.Find("Player");
+        _grass = obj.GetComponent<Grass>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _count = _grass._targetNumbers.Count;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("óvëfêî");
+        Debug.Log(_count);
+        if ((_count == 18) || (_count == 39) || (_count == 65) || (_count == 77) ||(_count == 79))
+            Invoke(("NextAnchor"), 0.5f);
+
+    }
+
+    private void NextAnchor()
+    {
+        _endNumberGameObject.SetActive(false);
+        _nextNumberGameObject.SetActive(true);
+        Debug.Log("ñÓàÛ");
+    }
+}
